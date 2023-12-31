b8b9bf683b88c3bb6726836b9ccfd99776531fff
hiromuu <hiromu2427@gmail.com>
2023-12-31 19:06:50 +0900

    画像が回転しない

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,81 @@
 import React from 'react';
 
 function App() {
-    const appStyle: React.CSSProperties = { // CSSプロパティの型を指定
+    // 背景画像のスタイル
+    const backgroundImageStyle: React.CSSProperties = {
+        width: '100%',
+        height: 'auto',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: -1, // 背景色よりも後ろに配置
+    };
+
+    // logo.svg のスタイル
+    const logoStyle: React.CSSProperties = {
+        position: 'absolute', // 絶対位置指定
+        left: '171.3147px', // x座標
+        top: '329.7607px', // y座標
+        width: '494.6333px', // 幅
+        height: '279.1613px', // 高さ
+        transform: 'rotate(7.72deg)', // 7.72度回転
+        transformOrigin: 'top left',// 回転の基点を左上に設定
+    };
+
+    // day.svg のスタイル
+    const dayStyle: React.CSSProperties = {
+        height: '60px',
+        position: 'absolute',
+        top: '30%', // 位置を調整
+        left: '40%',
+    };
+
+    // copy.svg のスタイル
+    const copyStyle: React.CSSProperties = {
+        height: '40px',
+        position: 'absolute',
+        top: '40%', // 位置を調整
+        left: '60%',
+    };
+
+    // Appコンポーネントのスタイル
+    const appStyle: React.CSSProperties = {
         textAlign: 'center',
-        backgroundColor: '#282c34',
+        backgroundColor: 'rgb(234, 96, 142)',
+        backgroundImage: '/images/bg.png',
         color: 'white',
         minHeight: '100vh',
         display: 'flex',
-        flexDirection: 'column', // この値は文字列リテラル型として認識される
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 'calc(10px + 2vmin)'
-    };
+        fontSize: 'calc(10px + 2vmin)',
+        position: 'relative',
+        margin: 0,
+        padding: 0,
+        zIndex: -2,
 
-    const headerStyle: React.CSSProperties = { // こちらも同様に
-        backgroundImage: 'url(./path/to/your/image.jpg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'fixed',
-        width: '100%',
-        height: '100%'
     };
 
     return (
-        <div style={appStyle}>
-            <header style={headerStyle}>
-                <p>Welcome to My Homepage!</p>
-            </header>
-        </div>
+        <React.Fragment>
+            <style>
+                {`
+                    html, body, #root {
+                        margin: 0;
+                        padding: 0;
+                        height: 100%;
+                        width: 100%;
+                    }
+                `}
+            </style>
+            <div style={appStyle}>
+                <img src="/images/main_pic.png" alt="Main Pic" style={backgroundImageStyle} />
+                <img src="/images/logo.svg" alt="Logo" style={logoStyle} />
+                <img src="/images/day.svg" alt="Day" style={dayStyle} />
+                <img src="/images/copy.svg" alt="Copy" style={copyStyle} />
+            </div>
+        </React.Fragment>
     );
 }
 
