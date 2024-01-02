4a0b5ecbe014a5e0a42a1e0ccfd0b3a4e5cee7c8
hiromuu <hiromu2427@gmail.com>
2024-01-02 16:09:54 +0900

    表紙のレイアウトはそれなり

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 function App() {
-    // 背景画像のスタイル
-    const backgroundImageStyle: React.CSSProperties = {
+    const containerStyle: React.CSSProperties = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
+    const textStyle: React.CSSProperties = {
+        textAlign: 'center',
+        color: 'rgb(35, 39, 47)',
+        padding: '20px',
+    };
+    // メイン画像のスタイル
+    const mainStyle: React.CSSProperties = {
         width: '100%',
         height: 'auto',
         position: 'absolute',
@@ -13,68 +25,80 @@ function App() {
 
     // logo.svg のスタイル
     const logoStyle: React.CSSProperties = {
-        position: 'absolute', // 絶対位置指定
-        left: '171.3147px', // x座標
-        top: '329.7607px', // y座標
-        width: '494.6333px', // 幅
-        height: '279.1613px', // 高さ
-        transform: 'rotate(7.72deg)', // 7.72度回転
-        transformOrigin: 'top left',// 回転の基点を左上に設定
+        position: 'absolute',
+        left: '15vw',
+        top: '110vh',
+        width: '30vw', // ビューポート幅の25%に設定
+        height: '40vh', // ビューポート高さの15%に設定
     };
 
-    // day.svg のスタイル
     const dayStyle: React.CSSProperties = {
-        height: '60px',
         position: 'absolute',
-        top: '30%', // 位置を調整
-        left: '40%',
+        left: '45vw',
+        top: '115vh',
+        width: '25vw', // ビューポート幅の15%に設定
+        height: '20vh', // ビューポート高さの10%に設定
     };
 
-    // copy.svg のスタイル
     const copyStyle: React.CSSProperties = {
-        height: '40px',
         position: 'absolute',
-        top: '40%', // 位置を調整
-        left: '60%',
+        left: '75%',
+        top: '13Vh',
+        height: '125Vh',
+        width: '20Vw',
+
     };
 
-    // Appコンポーネントのスタイル
-    const appStyle: React.CSSProperties = {
-        textAlign: 'center',
-        backgroundColor: 'rgb(234, 96, 142)',
-        backgroundImage: '/images/bg.png',
-        color: 'white',
-        minHeight: '100vh',
+    const circleStyle: React.CSSProperties = {
         display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        fontSize: 'calc(10px + 2vmin)',
-        position: 'relative',
-        margin: 0,
-        padding: 0,
-        zIndex: -2,
-
+        width: '180px', // コンテナの幅に合わせる
+        height: '180px', // コンテナの幅と同じ値に設定する
+        borderRadius: '50%',
+        backgroundColor: 'white',
+        color: 'rgb(234, 96, 142)',
+        fontSize: '20px',
+        textAlign: 'center',
+        lineHeight: '30px',
+        position: 'absolute', // コンテナ内で絶対位置指定
+        top: '130Vh',
+        left: '65Vw',
     };
+
 
     return (
         <React.Fragment>
             <style>
                 {`
-                    html, body, #root {
-                        margin: 0;
-                        padding: 0;
-                        height: 100%;
-                        width: 100%;
-                    }
-                `}
+                html, body, #root {
+                    margin: 0;
+                    padding: 0;
+                    height: auto; 
+                    min-height: 100%; 
+                    width: auto;
+                    min-width: 100%;
+                    background-color: rgb(234, 96, 142);
+                    background-image: url(/images/bg.png);
+                }
+    `}
             </style>
-            <div style={appStyle}>
-                <img src="/images/main_pic.png" alt="Main Pic" style={backgroundImageStyle} />
+            <div style={containerStyle}>
+                <img src="/images/main_pic.png" alt="Main Pic" style={mainStyle} />
                 <img src="/images/logo.svg" alt="Logo" style={logoStyle} />
                 <img src="/images/day.svg" alt="Day" style={dayStyle} />
                 <img src="/images/copy.svg" alt="Copy" style={copyStyle} />
+                <div style={circleStyle}>
+                    ゲスト<br />
+                    comming soon
+                </div>
+                <p style={textStyle}>
+                    桜吹雪に誘われてたどりつくのは、<br />
+                    境内に広がる見たことのない景色。<br />
+                    そこでは、無名も有名も、年齢も性別も関係のない<br />
+                    「表現者たち」が春の訪れを祝って遊んでいます。</p>
             </div>
+
         </React.Fragment>
     );
 }
